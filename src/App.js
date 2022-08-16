a4c83670002a7c2704b0fc608bf2b1b267abade6
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-08-16 16:58:24 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,55 +3,54 @@ import './App.css';
 
 function App() {
 
-        const [calc, setCalc] = useState('')
-        const [result, setResult] = useState('')
+    const [calc, setCalc] = useState('')
+    const [result, setResult] = useState('')
 
-        const operator = ['/', '*', '+', '-', '.']
+    const operator = ['/', '*', '+', '-', '.']
 
-        const updateCalc = (value) => {
-          if(
-            operator.includes(value) && calc === '' ||
-            operator.includes(value) && operator.includes(calc.slice(-1))
-          ) { 
-            return;
-          }
-            setCalc(calc + value)
+    const updateCalc = (value) => {
+       if(
+          operator.includes(value) && calc === '' ||
+          operator.includes(value) && operator.includes(calc.slice(-1))
+       ) { 
+          return;
+        }
+          setCalc(calc + value)
             if(!operator.includes(value)) {
               setResult(eval((calc + value).toString()))
             }
 
         }
 
-          const calculate = () => {
-            setCalc(eval(calc).toString())
-          }
-
-          const deleteLast = () => {
-            if(calc == '') {
-              return
-            }
-              const value = calc.slice(0, -1)
-              setCalc(value)
-          }
-
-    const createDigits = () => {
-      const digits = []
-      for(let i = 1; i < 10; i++) {
-          digits.push(
-          <button 
-          onClick={() => updateCalc(i.toString())}
-          key={i}
-          >
-            {i}
-            </button>
-            )
+      const calculate = () => {
+        setCalc(eval(calc).toString())
       }
-      return digits
-    }
+
+      const deleteLast = () => {
+        if(calc == '') {
+            return
+        }
+          const value = calc.slice(0, -1)
+            setCalc(value)
+          }
+
+      const createDigits = () => {
+        const digits = []
+          for(let i = 1; i < 10; i++) {
+            digits.push(
+              <button 
+              onClick={() => updateCalc(i.toString())}
+              key={i}
+              >
+                {i}
+                </button>
+              )
+          }
+        return digits
+      }
 
 
   return (
-
     <>
       <div className='container'>
         <div className='calculator'>
